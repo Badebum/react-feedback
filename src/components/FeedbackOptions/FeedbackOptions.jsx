6c41e8010cd84d5723808d2bef469820d19215b4
Andrey <82231196+Badebum@users.noreply.github.com>
2021-11-04 14:21:29 +0300

    finale

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,24 +1,27 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './FeedbackOptions.module.css';
 
-
-const FeedbackOptions = ({options, onLeaveFeedback}) => {
-<div>
-   { options.map((name, index)=>
-    <button
-    key={index}
-    type="button"
-    onClick={()=> onLeaveFeedback({name})}>{name}</button>
-    )
-    }
-
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map((name, index) => (
+        <button
+          className={styles.button}
+          key={index}
+          type="button"
+          onClick={() => onLeaveFeedback({ name })}
+        >
+          {name}
+        </button>
+      ))}
     </div>
-
-}
+  );
+};
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string),
-    onLeaveFeedback: PropTypes.func,
-}
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func,
+};
 
-export default FeedbackOptions
+export default FeedbackOptions;
